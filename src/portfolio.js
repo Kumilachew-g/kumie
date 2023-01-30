5f2cdbeb770bdf13d1584b0775ec8bbedec8ae1e
Kumilachew-g <kumilachew1212@gmail.com>
2023-01-30 09:29:46 +0300

    Update texts based on reviews

--- src/portfolio.js
+++ src/portfolio.js
@@ -656,7 +656,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "photo-logo.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on almost every social media outlet. You can message me, and I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
   blogSection: {
     title: "Blogs",
